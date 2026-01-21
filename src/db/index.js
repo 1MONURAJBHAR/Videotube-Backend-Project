4df7a7965dbe524726aa83205770d06b5f875721
Monu Rajbhar <rajbharmonu436@gmail.com>
2026-01-22 02:08:07 +0530

    update the backend

--- src/db/index.js
+++ src/db/index.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(
           `${process.env.MONGODB_URI}/${DB_NAME}`
         );
-        console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`); // connectionInstance.connection gives the actual MongoDB connection. , .host tells you the hostname of the MongoDB server your app connected to.
+        console.log(`\n ✅ MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`); // connectionInstance.connection gives the actual MongoDB connection. , .host tells you the hostname of the MongoDB server your app connected to.
     } catch (error) {
-      console.log("MongoDB connection error ", error);
+      console.log("❌ MongoDB connection error ", error);
       process.exit(1); //exit from process no.1, read Nodejs Docs for more info || ChatGPT
       //process.exit(1) is a Node.js command used to immediately stop the running process.
     }
